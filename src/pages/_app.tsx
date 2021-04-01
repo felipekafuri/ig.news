b4f7fa25049ba0a97d438bb39e356207c81ea4a8
felipekafuri <felipekafuri@discente.ufg.br>
2021-04-01 00:52:38 -0300

    authentication using next-auth and github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 import '../styles/global.scss'
+import { Provider as NextAuthProvider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  // Esse session={pageProps.session} faz com que mesmo com f5 a sessions se mantenham
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
