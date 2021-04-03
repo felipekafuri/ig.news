d946d93d33a4a9c7e0071ab104f7c93e4b6f799a
felipekafuri <felipekafuri@discente.ufg.br>
2021-04-03 16:39:48 -0300

    add threatment to stripe webhooks responses

---
+++ src/pages/api/_lib/menageSubscription.ts
@@ -0,0 +1,43 @@
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+import { stripe } from '../../../services/stripe'
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  isCreateAction = false
+) {
+  // search in db for customer with customerId
+  const userRef = await fauna.query(
+    q.Select(
+      'ref',
+      q.Get(q.Match(q.Index('user_by_stripe_customer_id'), customerId))
+    )
+  )
+
+  // save subscription data to fauna db
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id
+  }
+
+  if (isCreateAction) {
+    await fauna.query(
+      q.Create(q.Collection('subscriptions'), { data: subscriptionData })
+    )
+  } else {
+    const getSubscriptionRef = q.Select(
+      'ref',
+      q.Get(q.Match(q.Index('subscription_by_id'), subscriptionId))
+    )
+    await fauna.query(
+      q.Replace(getSubscriptionRef, {
+        data: { subscriptionData }
+      })
+    )
+  }
+}
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { stripe } from '../../services/stripe'
 import { getSession } from 'next-auth/client'
+import { stripe } from '../../services/stripe'
+
 import { fauna } from '../../services/fauna'
 import { query as q } from 'faunadb'
 
@@ -9,6 +10,7 @@ type User = {
     id: string
   }
   data: {
+    email: string
     stripe_customer_id: string
   }
 }
@@ -17,20 +19,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req })
 
+    // check if customer exists on faunaDb
     const user = await fauna.query<User>(
       q.Get(q.Match(q.Index('user_by_email'), q.Casefold(session.user.email)))
     )
 
     let customerId = user.data.stripe_customer_id
-    // verifica se meu customer no database ja possui um stripe_customer_id
+
     if (!customerId) {
-      // criando customer no stripe
+      // create stripe customer
       const stripeCustomer = await stripe.customers.create({
         email: session.user.email
-        // metadata:
+        // metadata
       })
 
-      // Atualiza usuário no banco de dados adicionando à ele o id de customer do stripe
+      // save customer do faunaDb
       await fauna.query(
         q.Update(q.Ref(q.Collection('users'), user.ref.id), {
           data: {
@@ -42,17 +45,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       customerId = stripeCustomer.id
     }
 
-    // criando sessão de checkout stripe
+    // create stripe checkout
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: customerId, // id do customer no stripe
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
-      line_items: [
-        {
-          price: 'price_1Ib5V2IePKUqusNGIK4tGrjz',
-          quantity: 1
-        }
-      ],
+      line_items: [{ price: 'price_1Ib5V2IePKUqusNGIK4tGrjz', quantity: 1 }],
       mode: 'subscription',
       allow_promotion_codes: true,
       success_url: process.env.STRIPE_SUCCESS_URL,
@@ -62,7 +60,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(200).json({ sessionId: stripeCheckoutSession.id })
   } else {
     res.setHeader('Allow', 'POST')
-
-    res.status(405).end('Method not allowed.')
+    res.status(405).end('Method not allowed')
   }
 }
