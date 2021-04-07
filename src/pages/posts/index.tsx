2e39091369fa5dbe238f0337cadb9a53ff30d9eb
felipekafuri <felipekafuri@discente.ufg.br>
2021-04-07 18:54:43 -0300

    load more function

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -6,6 +6,7 @@ import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
 import Link from 'next/link'
 import { FiCalendar } from 'react-icons/fi'
+import { useState } from 'react'
 
 interface Post {
   slug: string
@@ -14,13 +15,52 @@ interface Post {
   updatedAt: string
 }
 
-interface PostsProps {
-  posts: Post[]
+interface PostPagination {
+  next_page: string
+  results: Post[]
 }
 
-export default function Posts({ posts }: PostsProps) {
-  // TODO Load more function
+interface PostsProps {
+  postsPagination: PostPagination
+}
 
+export default function Posts({ postsPagination }: PostsProps) {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results)
+  const [nextPage, setNextPage] = useState<string | null>(
+    postsPagination.next_page
+  )
+
+  const handleLoadMore = async () => {
+    const headers = new Headers()
+    const init = {
+      method: 'GET',
+      headers
+    }
+    if (nextPage) {
+      fetch(nextPage, init)
+        .then(response => response.json())
+        .then(data => {
+          const newPosts = data.results.map(post => {
+            return {
+              slug: post.uid,
+              title: RichText.asText(post.data.title),
+              excerpt:
+                post.data.content.find(content => content.type === 'paragraph')
+                  ?.text ?? '',
+              updatedAt: new Date(
+                post.last_publication_date
+              ).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+              })
+            }
+          })
+          setPosts([...posts, ...newPosts])
+          setNextPage(data.next_page)
+        })
+    }
+  }
   return (
     <>
       <Head>
@@ -29,7 +69,7 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <Link href={`/posts/${post.slug}`} key={post.slug}>
+            <Link href={`/posts/preview/${post.slug}`} key={post.slug}>
               <a>
                 <time>
                   <FiCalendar /> {post.updatedAt}
@@ -39,6 +79,13 @@ export default function Posts({ posts }: PostsProps) {
               </a>
             </Link>
           ))}
+          {nextPage ? (
+            <button className={styles.loadMore} onClick={handleLoadMore}>
+              Carregar mais posts
+            </button>
+          ) : (
+            <></>
+          )}
         </div>
       </main>
     </>
@@ -50,7 +97,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const response = await prismic.query(
     [Prismic.predicates.at('document.type', 'post')],
-    { fetch: ['post.title', 'post.content'], pageSize: 100 }
+    { fetch: ['post.title', 'post.content'], pageSize: 1 }
   )
 
   const posts = response.results.map(post => {
@@ -73,7 +120,11 @@ export const getStaticProps: GetStaticProps = async () => {
 
   return {
     props: {
-      posts
-    }
+      postsPagination: {
+        results: posts,
+        next_page: response.next_page
+      }
+    },
+    revalidate: 60 * 60 // 1 hour
   }
 }
